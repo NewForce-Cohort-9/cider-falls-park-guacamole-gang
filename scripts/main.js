4a30e4af03f70784b63406467ecaf465c387faa9
Meowph <bpardue33@gmail.com>
2024-05-03 10:57:50 -0400

    edited main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-import { getGuests } from "./guestsList.js"
-import { getSections } from "./sectionsList.js"
-import { getServices } from "./database.js"
-import { getAttractions } from "./database.js"
-import { getServiceSections } from "./database.js"
-import { getAttractionSections } from "./database.js"
+import { Guests} from "./guestsList.js"
+import { sectionsList } from "./sectionsList.js"
 
-const mainContainer = document.querySelector("#container")
+
+const sectionContainer = document.querySelector("#sectionContainer")
 
 const applicationHTML = `
 <h1>Cider Falls Park</h1>
@@ -14,7 +10,7 @@ const applicationHTML = `
 <article class="detail--right aligned list">
     <section class="guestList">
         <h2>Guests</h2>
-        ${getGuests()}
+        ${Guests()}
     </section>
 </article>
 
@@ -22,42 +18,8 @@ const applicationHTML = `
 <article class="detail--main grid">
     <section class="sectionsList">
         <h2>Sections</h2>
-        ${getSections()}
-    </section>
-
-    <section class="servicesList">
-        <h3>Services</h3>
-        ${getServices()}
-    </section>
-    
-    <section class="attractionsList">
-       <h3></h3>
-       ${getAttractions()}
-    </section>
-   </article>
-
-
-   <article class="detail--clickable services list ">
-    <section class="serviceSection">
-        <h2>Guests</h2>
-        ${getServiceSections()}
-    </section>
-
-    <section class="attractionsSection">
-        <h2>Guests</h2>
-        ${getAttractionSections()}
+        ${sectionsList()}
     </section>
 </article>
-
 `
-
-mainContainer.innerHTML = applicationHTML
-=======
-import { sectionsList } from "./sectionsList.js";
-
-const sectionContainer = document.querySelector("#sectionContainer")
-
-const applicationHTML = `${sectionsList()}`
-
 sectionContainer.innerHTML = applicationHTML
->>>>>>> main
