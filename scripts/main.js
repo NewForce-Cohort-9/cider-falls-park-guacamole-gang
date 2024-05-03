ae5689a0edfa22702413ce2ba15fef489c83ab66
Meowph <bpardue33@gmail.com>
2024-05-03 14:10:18 -0400

    saving small css changes

--- scripts/main.js
+++ scripts/main.js
@@ -5,20 +5,19 @@ import { sectionsList } from "./sectionsList.js"
 const sectionContainer = document.querySelector("#sectionContainer")
 
 const applicationHTML = `
-<h1>Cider Falls Park</h1>
+<h1 id="title">Cider Falls Park</h1>
 
-<article class="detail--right aligned list">
-    <section class="guestList">
-        <h2>Guests</h2>
-        ${Guests()}
-    </section>
-</article>
-
-
-<article class="detail--main grid">
+<article class="detail--main--grid">
     <section class="sectionsList">
         <h2>Sections</h2>
         ${sectionsList()}
+    </section>
+</article>
+
+<article class="detail--right--aligned list">
+    <section class="guestList">
+        <h2>Guests</h2>
+        ${Guests()}
     </section>
 </article>
 `
