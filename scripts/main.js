18a25756f184616024c59af397611ac40baaf28c
Meowph <bpardue33@gmail.com>
2024-05-05 11:15:21 -0400

    save

--- scripts/main.js
+++ scripts/main.js
@@ -5,19 +5,22 @@ import { sectionsList, servicesList } from "./sectionsList.js"
 const sectionContainer = document.querySelector("#sectionContainer")
 
 const applicationHTML = `
-<h1 id="title">Cider Falls Park</h1>
+<h1>Cider Falls Park</h1>
+<article class="serviceList">
+${servicesList()}
+</article>
+<article class="detail--right aligned list">
+    <section class="guestList">
+        <h2>Guests</h2>
+        ${Guests()}
+    </section>
+</article>
+
 
 <article class="detail--main grid">
     <section class="sectionsList">
         <h2>Sections</h2>
         ${sectionsList()}
-    </section>
-</article>
-
-<article class="detail--right--aligned list">
-    <section class="guestList">
-        <h2>Guests</h2>
-        ${Guests()}
     </section>
 </article>
 `
